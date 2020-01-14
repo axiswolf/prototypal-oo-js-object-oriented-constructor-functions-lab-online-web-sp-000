2dcffeff6a696dc53887ea57b084ae4e87da2f39
Karen V <karenvo@usc.edu>
2020-01-14 09:13:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 class Scooter {
-  constructor() {
-
+  constructor(year, color, model) {
+    this.year = year
+    this.color = color
+    this.model = model
   }
 }
 
 class Driver {
-  constructor() {
-
+  constructor(name, age, experience) {
+    this.name=name
+    this.age = age
+    this.experience = experience
   }
 }
 
